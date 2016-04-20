43fa13f7ca94d0c8247c7ada722543a19721c1df
Jacob Zwickler <jzwickl1@cs.binghamton.edu>
2016-04-20 19:23:23 -0400

    added proj1

---
+++ proj1/proj1.h
@@ -0,0 +1,21 @@
+#ifndef lab5_h
+#define lab5_h
+
+
+int* pageTables[1000];
+int *globalHistory;
+int procList[1000];
+int freeSpace = 0;
+int  MAXSPACE = 0; 
+int faults = 0;
+double totalRef = 0;
+double fRate = 0;
+
+void validateArgs(char* arg1, char* arg2);
+int  validateProc(int procNum, char mode);
+void initializeList();
+void start(int procNum, int addrSz);
+void terminate(int procNum);
+void refer(int procNum,int  vpn);
+
+#endif 
